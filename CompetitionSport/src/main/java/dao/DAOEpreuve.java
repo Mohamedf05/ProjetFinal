f3b6aae1d84e489a6caaa30b4b0d2eb3c48680c6
Mohamed Fofana <mohamedf3005@gmail.com>
2022-03-04 16:22:05 +0100

    IDAOEpreuve

---
+++ CompetitionSport/src/main/java/dao/DAOEpreuve.java
@@ -0,0 +1,39 @@
+package dao;
+
+import java.util.List;
+
+import model.Epreuve;
+
+public class DAOEpreuve implements IDAOEpreuve{
+
+	@Override
+	public Epreuve findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Epreuve> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Epreuve insert(Epreuve o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Epreuve o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
