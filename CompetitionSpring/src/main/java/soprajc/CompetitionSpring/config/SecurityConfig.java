0430d2c152a7024b45857fa2a804b97464fd3e26
Michel Vong <vong.mic@gmail.com>
2022-04-08 14:55:27 +0200

    HttpMethod.OPTIONS
    .antMatchers dans SecurityConfig

--- CompetitionSpring/src/main/java/soprajc/CompetitionSpring/config/SecurityConfig.java
+++ CompetitionSpring/src/main/java/soprajc/CompetitionSpring/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().ignoringAntMatchers("/api/**")
 			.and()
 			.authorizeHttpRequests()
+				.antMatchers(HttpMethod.OPTIONS).permitAll()
 				.antMatchers(HttpMethod.POST).permitAll()
 				.antMatchers(HttpMethod.GET,"/api/athlete").permitAll()
 				.antMatchers("/api/athlete/**").hasRole("ATHLETE")
