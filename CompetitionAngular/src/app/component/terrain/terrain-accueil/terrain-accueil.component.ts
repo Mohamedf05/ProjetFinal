1995fbd1141048778bfb09a0fbdfa63c0d52e30f
Michel Vong <vong.mic@gmail.com>
2022-04-12 14:39:40 +0200

    Component Terrain List et Edit

--- CompetitionAngular/src/app/component/terrain/terrain-accueil/terrain-accueil.component.ts
+++ CompetitionAngular/src/app/component/terrain/terrain-accueil/terrain-accueil.component.ts
@@ -1,15 +1,31 @@
+import { Adresse } from './../../../model/adresse';
+import { Terrain } from './../../../model/terrain';
+import { TerrainService } from './../../../services/terrain.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-terrain-accueil',
   templateUrl: './terrain-accueil.component.html',
-  styleUrls: ['./terrain-accueil.component.css']
+  styleUrls: ['./terrain-accueil.component.css'],
 })
 export class TerrainAccueilComponent implements OnInit {
+  terrains: Terrain[] = [];
 
-  constructor() { }
+  constructor(private terrainService: TerrainService) {}
 
   ngOnInit(): void {
+    this.list();
   }
 
+  list() {
+    this.terrainService.getAll().subscribe((result) => {
+      this.terrains = result;
+    });
+  }
+
+  delete(id: number) {
+    this.terrainService.delete(id).subscribe((noResult) => {
+      this.list();
+    });
+  }
 }
